50223a08619cf4630b001874f243c60176751b29
Hannah Coyle <hannah.o.coyle@gmail.com>
2022-02-24 13:42:21 +0000

    Finished playRound function

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const rockPaperScissors = ["rock", "paper", "scissors"];
+const rockPaperScissors = ["Rock", "Paper", "Scissors"];
 
 function computerPlay(rockPaperScissors) {
     return rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
 }
 
-const playerSelection = "Rock";
+let playerSelection = "Rock";
 const computerSelection = computerPlay(rockPaperScissors);
 
+playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection)
-        return "It's a draw!";
+    if (playerSelection === computerSelection)
+        return `It's a draw! You both picked ${playerSelection}`;
+    else if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock")
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    else
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    
 }
 
 console.log(playRound(playerSelection, computerSelection));
