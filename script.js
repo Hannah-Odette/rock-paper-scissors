1e43a2c96d61747e20f052940acdd3e038f63ed2
Hannah Coyle <hannah.o.coyle@gmail.com>
2022-02-27 14:02:16 +0000

    Added function 'game'

--- script.js
+++ script.js
@@ -4,19 +4,44 @@ function computerPlay(rockPaperScissors) {
     return rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
 }
 
-let playerSelection = "Rock";
+let playerSelection = prompt("Rock, paper, scissors?");
 const computerSelection = computerPlay(rockPaperScissors);
 
 playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection)
-        return `It's a draw! You both picked ${playerSelection}`;
+        return "draw";
     else if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock")
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return "computer";
     else
-        return `You win! ${playerSelection} beats ${computerSelection}`;
-    
+        return "player";   
 }
 
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+        let result = playRound(playerSelection, computerSelection);
+        if(result === "player") {
+            console.log (`You win! ${playerSelection} beats ${computerSelection}`);
+            playerPoints++;
+        }
+        else if (result === "computer") {
+            console.log (`You lose! ${computerSelection} beats ${playerSelection}`);
+            computerPoints++;
+        }
+        else {
+            console.log (`It's a draw! You both picked ${playerSelection}`);
+        }
+    }
+    if (playerPoints === computerPoints)
+        return `It's a draw: ${playerPoints} ${computerPoints}`;
+    else if (playerPoints > computerPoints)
+        return `You win: ${playerPoints} ${computerPoints}`;
+    else
+        return `You lose: ${playerPoints} ${computerPoints}`;
+}
+
+console.log(game());
