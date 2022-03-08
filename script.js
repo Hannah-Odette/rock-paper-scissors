89ebf19a533c6ad297cc04f34d0002370530e931
Hannah Coyle <hannah.o.coyle@gmail.com>
2022-03-08 13:36:45 +0000

    Added to script.js to allow player choice between rounds and make a different computer choice. Winner is then declared

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ function computerPlay(rockPaperScissors) {
     return rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
 }
 
-let playerSelection = prompt("Rock, paper, scissors?");
-const computerSelection = computerPlay(rockPaperScissors);
-
-playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+let playerSelection = "Rock";
+let computerSelection = computerPlay(rockPaperScissors);
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection)
@@ -22,26 +20,29 @@ function game() {
     let playerPoints = 0;
     let computerPoints = 0;
     for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Rock, paper, scissors?");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+        computerSelection = computerPlay(rockPaperScissors);
         playRound(playerSelection, computerSelection);
         let result = playRound(playerSelection, computerSelection);
         if(result === "player") {
-            console.log (`You win! ${playerSelection} beats ${computerSelection}`);
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
             playerPoints++;
         }
         else if (result === "computer") {
-            console.log (`You lose! ${computerSelection} beats ${playerSelection}`);
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
             computerPoints++;
         }
         else {
-            console.log (`It's a draw! You both picked ${playerSelection}`);
+            console.log(`It's a draw! You both picked ${playerSelection}`);
         }
     }
     if (playerPoints === computerPoints)
-        return `It's a draw: ${playerPoints} ${computerPoints}`;
+        console.log(`It's a draw: ${playerPoints} ${computerPoints}`);
     else if (playerPoints > computerPoints)
-        return `You win: ${playerPoints} ${computerPoints}`;
+        console.log(`You win: ${playerPoints} ${computerPoints}`);
     else
-        return `You lose: ${playerPoints} ${computerPoints}`;
+        console.log(`You lose: ${playerPoints} ${computerPoints}`);
 }
 
-console.log(game());
+game();
